1655b75a490991f98be502f0ea82cbbeb0c20e19
Curtis Schlak <curtis.schlak@theironyard.com>
2017-04-19 04:13:11 -0700

    Add a task manager to contain the logic about task management.

--- VerySimpleTaskList/Program.cs
+++ VerySimpleTaskList/Program.cs
@@ -1,66 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace VerySimpleTaskList
+﻿namespace VerySimpleTaskList
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<Task> tasks = new List<Task>();
-
-            while (true)
-            {
-                Console.WriteLine("Add, prioritize or complete a task, or exit? (add/mark/p/exit)");
-                string choice = Console.ReadLine();
-                if (choice == "exit")
-                {
-                    break;
-                }
-
-                if (choice == "add")
-                {
-                    Console.WriteLine("What is your task?");
-                    string description = Console.ReadLine();
-                    Task task = new Task(description);
-                    tasks.Add(task);
-
-                    Console.WriteLine("YOUR TASKS:");
-                    Console.WriteLine("----------------------------------");
-                    foreach (Task t in tasks)
-                    {
-                        Console.WriteLine(t.DescribeYourself());
-                    }
-                }
-                else if (choice == "mark")
-                {
-                    for (int i = 0; i < tasks.Count; i += 1)
-                    {
-                        Task t = tasks[i];
-                        string desc = t.DescribeYourself();
-                        Console.WriteLine($"{i}. {desc}");
-                        // Console.WriteLine($"{i}. {tasks[i].DescribeYourself()}");
-                    }
-                    Console.WriteLine("Which task would you like to complete?");
-                    string input = Console.ReadLine();
-                    int num = int.Parse(input);
-                    tasks[num].MarkCompleted();
-                }
-                else if (choice == "p")
-                {
-                    for (int i = 0; i < tasks.Count; i += 1)
-                    {
-                        Console.WriteLine($"{i}. {tasks[i].DescribeYourself()}");
-                    }
-                    Console.WriteLine("Which task would you like to prioritize?");
-                    string input = Console.ReadLine();
-                    int num = int.Parse(input);
-                    Console.WriteLine("What is the task's priority?");
-                    input = Console.ReadLine();
-                    int priority = int.Parse(input);
-                    tasks[num].SetPriority(priority);
-                }
-            }
+            TaskManager manager = new TaskManager();
+            manager.Run();
         }
     }
 }
---
+++ VerySimpleTaskList/TaskManager.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VerySimpleTaskList
+{
+    public class TaskManager
+    {
+        public TaskManager()
+        {
+            _tasks = new List<Task>();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                ShowMenu();
+                int choice = GetNumberFromUser();
+
+                if (choice == 0)
+                {
+                    break;
+                }
+                else if (choice == 1)
+                {
+                    DoAddTask();
+                }
+                else if (choice == 2)
+                {
+                    DoMarkTaskComplete();
+                }
+                else if (choice == 3)
+                {
+                    DoSetPriority();
+                }
+                else if (choice == 4)
+                {
+                    DoListAllTasks();
+                }
+            }
+        }
+
+        private void DoListAllTasks()
+        {
+            Console.Clear();
+            Console.WriteLine("YOUR TASKS");
+            Console.WriteLine("-------------------------");
+            PrintNumberedTaskList();
+            Console.WriteLine("-------------------------");
+            Console.Write("Press Enter to return to the menu...");
+            Console.ReadLine();
+        }
+
+        private void DoSetPriority()
+        {
+            Console.Clear();
+            Console.WriteLine("CHANGE TASK PRIORITY");
+            Console.WriteLine("-------------------------");
+            PrintNumberedTaskList();
+            Console.WriteLine("-------------------------");
+            Console.Write("What task do you want to change? ");
+
+            int index = GetNumberFromUser();
+
+            Console.Write("What is the new task's priority? ");
+
+            int newPriority = GetNumberFromUser();
+
+            _tasks[index].SetPriority(newPriority);
+        }
+
+        private void DoMarkTaskComplete()
+        {
+            Console.Clear();
+            Console.WriteLine("COMPLETE A TASK");
+            Console.WriteLine("-------------------------");
+            PrintNumberedTaskList();
+            Console.WriteLine("-------------------------");
+            Console.Write("What task did you complete? ");
+
+            int index = GetNumberFromUser();
+            _tasks[index].MarkCompleted();
+        }
+
+        private void PrintNumberedTaskList()
+        {
+            for (int i = 0; i < _tasks.Count; i += 1)
+            {
+                Task task = _tasks[i];
+                Console.WriteLine($"{i}. {task.DescribeYourself()}");
+            }
+        }
+
+        private void DoAddTask()
+        {
+            Console.Clear();
+            Console.WriteLine("ADD A TASK");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("What is your next task?");
+
+            string description = GetStringFromUser();
+            Task newTask = new Task(description);
+            _tasks.Add(newTask);
+        }
+
+        private string GetStringFromUser()
+        {
+            return Console.ReadLine();
+        }
+
+        private int GetNumberFromUser()
+        {
+            string input = Console.ReadLine();
+            return int.Parse(input);
+        }
+
+        private void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("TASK MANAGEMENT!");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1. Add a task");
+            Console.WriteLine("2. Mark a task complete");
+            Console.WriteLine("3. Set a task's priority");
+            Console.WriteLine("4. List the tasks");
+            Console.WriteLine();
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("-------------------------");
+            Console.Write("What would you like to do? ");
+        }
+
+        private List<Task> _tasks;
+    }
+}
