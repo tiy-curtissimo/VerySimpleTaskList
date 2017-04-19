8d124e64dd9e81ea11107cce569bb8625918a489
Curtis Schlak <curtis.schlak@theironyard.com>
2017-04-19 03:54:27 -0700

    End of class.

---
+++ VerySimpleTaskList/Task.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace VerySimpleTaskList
+{
+    public class Task
+    {
+        public Task(string description)
+        {
+            _description = description;
+            _priority = 1;
+            _completed = false;
+        }
+
+        public void SetPriority(int priority)
+        {
+            if (priority >= 1 && priority <= 5)
+            {
+                _priority = priority;
+            }
+        }
+
+        public void MarkCompleted()
+        {
+            _completed = true;
+        }
+
+        public string DescribeYourself()
+        {
+                        // This is called "string interpolation"
+            string me = $"I am a task to {_description} with priority {_priority}";
+            if (_completed)
+            {
+                me += " and I am done";
+            }
+            else
+            {
+                me += " and I am not done";
+            }
+            return me;
+        }
+
+        private string _description;
+        private int _priority;
+        private DateTime _dueDate;
+        private bool _completed;
+    }
+}
