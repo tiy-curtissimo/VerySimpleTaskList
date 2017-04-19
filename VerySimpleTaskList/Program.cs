287fe45de9ec45fb06bc2f303e06ea549baa98af
IEUser <IEUser@IE11WIN8_1>
2017-04-19 07:25:59 -0700

    Added a welcome message.

--- VerySimpleTaskList/Program.cs
+++ VerySimpleTaskList/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace VerySimpleTaskList
+﻿using System;
+
+namespace VerySimpleTaskList
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("WELCOME!");
             TaskManager manager = new TaskManager();
             manager.Run();
         }
